bddc57c39b60cc66f3b907fa5a000f675ad9fc16
Peter Kim <peter.kim@buzzvil.com>
2020-10-03 20:01:03 +0900

    add nerdtree vim plugin

--- install.sh
+++ install.sh
@@ -4,3 +4,7 @@ DIR=$(pwd)
 
 # Symlink dot files
 ln -s $DIR/vimrc ~/.vimrc
+
+# Install vim plugins (assumes that vim version >= 8)
+git clone https://github.com/preservim/nerdtree.git ~/.vim/pack/vendor/start/nerdtree
+vim -u NONE -c "helptags ~/.vim/pack/vendor/start/nerdtree/doc" -c q
